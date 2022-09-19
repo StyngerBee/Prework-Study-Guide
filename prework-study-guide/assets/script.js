f5646712d6ff06960592f2df09a32f3378a1afd4
StyngerBee <cdsonics@yahoo.com>
2022-09-18 23:17:25 -0400

    git commit -a

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,5 +1,12 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-/*
+
+function listTopics() {
+    for(var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
+    }
+
+function selectTopic() {
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -11,7 +18,5 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
-*/
-for(var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
 }
+
